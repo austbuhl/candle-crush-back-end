f8a8faba0762d7dcfa5068807e82f5bcfc694b35
AB <austinbuhler@gmail.com>
2020-11-05 10:55:11 -0500

    Routes and CORS setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+user1 = User.create(username: 'test', password: 'test')
+
+20.times do 
+  Candle.create(name: Faker::Hipster.word, price: rand(100.00), image: Faker::LoremFlickr.image,description: Faker::Hipster.paragraph, scent: Faker::Hipster.word)
+end
+
+5.times do 
+  Purchase.create(user_id: user1.id, candle_id: Candle.all.sample.id)
+end
