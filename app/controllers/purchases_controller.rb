a99dd7db37cbb2b1cfe323f48de7fc596f8f5a5a
AB <austinbuhler@gmail.com>
2020-11-05 16:28:31 -0500

    Added Purchases Controller

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,17 @@
+class PurchasesController < ApplicationController
+
+  def index
+    purchases = Purchase.all
+    render json: purchases, except: [:created_at, :updated_at]
+  end
+
+  def create
+
+    params[:candle].each do |purchase| 
+      Purchase.create(user_id: 1, candle_id: purchase[:id])
+    end
+
+    byebug
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/candles', to: 'candles#index'
   get '/users', to: 'users#index'
+  post '/purchases', to: 'purchases#create'
+  get '/purchases', to: 'purchases#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
